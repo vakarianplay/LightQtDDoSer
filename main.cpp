801abec83591cf590d4692af193f19e6d04ae233
Kostya Vakarian <vakarianplay@gmail.com>
2022-03-04 10:39:24 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,10 +5,10 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    QFile styleFile(":/images/appstyle.qss");
-    styleFile.open(QFile::ReadOnly);
-    QString style(styleFile.readAll());
-    a.setStyleSheet(style);
+    //QFile styleFile(":/images/appstyle.qss");
+    //styleFile.open(QFile::ReadOnly);
+    //QString style(styleFile.readAll());
+    //a.setStyleSheet(style);
     MainWindow w;
     w.show();
     return a.exec();
